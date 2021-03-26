169c4807007c7bdec5abe7568b7960e4d45534b4
Vinay Billa <billavinay2012@gmail.com>
2021-03-26 18:12:31 +0530

    Brute Force Password Cracker

---
+++ Brute Force Password Detctor/code.py
@@ -0,0 +1,18 @@
+import pyautogui
+import string
+import random
+
+chars=string.printable
+char_list= list(chars)
+
+actual_password = pyautogui.password("Enter your password:")
+answer=""
+
+while answer!=list(actual_password):
+    answer = random.choices(char_list, k=len(actual_password))
+    print("<<<<<<<<<<<<<<<",answer,">>>>>>>>>>>>>>>>")
+
+pyautogui.alert(text=''.join(answer),title="Cracked Password ",button='OK')
+
+
+
