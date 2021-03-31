981da3d01d2a80a723087294a705e9a001b39c2d
Vinay Billa <billavinay2012@gmail.com>
2021-03-31 20:35:53 +0530

    Generated hard word list by hard words text file

---
+++ HangmanGame/generate_hard_words.py
@@ -0,0 +1,14 @@
+hard_word_list = []
+f=open("HangmanGame\\hard_words.txt", "r")
+
+while True:
+    n = f.readline()
+    if n=='zombie':
+        hard_word_list.append('zombie')
+        break
+    hard_word_list.append(n[:len(n)-1])
+print(hard_word_list)
+
+
+
+
