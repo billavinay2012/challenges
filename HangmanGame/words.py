c0e7133ee18d4a7a8e884e153403b9dbb6a1c6cb
Vinay Billa <billavinay2012@gmail.com>
2021-03-31 21:22:29 +0530

    Added choice() method

--- HangmanGame/words.py
+++ HangmanGame/words.py
@@ -3,56 +3,70 @@ import string
 from generate_easy_words import easy_word_list
 from generate_medium_words import medium_word_list
 from generate_hard_words import hard_word_list
+    
+def choice():
+    while True:
+        print("\nChoose the difficulty level to continue or End the game(4)")
+        choice = int(input("1.Easy 2.Medium 3.Hard 4.Exit"))
+        if choice == 1:
+            return list(random.choice(easy_word_list).upper())
+        elif choice==2:
+            return list(random.choice(medium_word_list).upper())
+        elif choice==3:
+            return list(random.choice(hard_word_list).upper())
+        elif choice==4:
+            exit(0)
+        else:
+            print("Please enter a valid choice:")
 
-def changes(element,index):
-    global answer
-    answer[index]=element
+def game():
 
-#read from CommonWord.txt file on google and converted to list using file open() and readline()
-list_of_words=['abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes', 'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 
-'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 
-'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac', 'zombie']
+    #choosing a word as the question
+    word_to_be_guessed=choice()
 
-#choosing a word as the question
-word_to_be_guessed = list(random.choice(list_of_words).upper())
-print(''.join(word_to_be_guessed))
+    #creating blank spaces for user
+    answer=[]
+    for i in range(len(word_to_be_guessed)):
+        answer.append('—')
+    print(' '.join(answer))
 
-#creating blank spaces for user
-answer=[]
-for i in range(len(word_to_be_guessed)):
-    answer.append('—')
-print(' '.join(answer))
+    #Total number of lives
+    lives = len(word_to_be_guessed)
 
+    used_word_set=set()
 
-#Total number of lives
-lives = len(word_to_be_guessed)
+    while lives>0 and ''.join(answer)!=''.join(word_to_be_guessed):
+        print(f"\nYou have {lives} lives left")
+        element = input("Enter the letter for word:  ").upper()
 
-used_word_set=set()
+        if element in used_word_set:
+            print(f"You have already used {element}")
+        elif element in word_to_be_guessed:
+            for i in word_to_be_guessed:
+                if element==i:
+                    answer[word_to_be_guessed.index(i)]=i
+            print(' '.join(answer))
+            used_word_set.add(element)
+        else:
+            used_word_set.add(element)
+            print("Oops! wrong guess...")
+            print(' '.join(answer))
+            lives-=1    
+        print(f"Used letters are: {used_word_set}")
 
-while lives>0 and ''.join(answer)!=''.join(word_to_be_guessed):
-    print(f"\nYou have {lives} lives left")
-    element = input("Enter the letter for word:  ").upper()
-
-    if element in used_word_set:
-        print(f"You have already used {element}")
-    elif element in word_to_be_guessed:
-        used_word_set.add(element)
-        for i in word_to_be_guessed:
-            if element==i:
-                changes(element,word_to_be_guessed.index(i))
-        print(' '.join(answer))
+    word_to_be_guessed=''.join(word_to_be_guessed)
+    if lives>0:
+        print("Congrats you have correctly guessed ",word_to_be_guessed)
     else:
-        used_word_set.add(element)
-        print("Oops! wrong guess...")
-        print(' '.join(answer))
-        lives-=1    
-    print(f"Used letters are: {used_word_set}")
+        print("Oh no! The correct word is ",word_to_be_guessed)
+    choice()
 
-if lives>0:
-    print("Congrats you have correctly guessed ".join(word_to_be_guessed))
-else:
-    print("Oh no! The correct word is "+str(word_to_be_guessed))
 
+        
+print("|   |  /\  |\  | |‾‾‾‾‾‾ |\    /|  /\  |\  |      |‾‾‾‾‾‾  /\  |\    /||‾‾‾‾‾‾ ")
+print("|———| /——\ | \ | |  |‾‾| | \  / | /——\ | \ |      |  |‾‾| /——\ | \  / ||—————  ")
+print("|   |/    \|  \| |_____| |  \/  |/    \|  \|      |_____|/    \|  \/  ||______ ")
+game()
 
 
 
